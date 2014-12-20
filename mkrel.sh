5172cf384437094de31682ec62885f7b55bb4ea4
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2014-12-20 16:14:02 +0100

    misc: update release script

--- mkrel.sh
+++ mkrel.sh
@@ -1,20 +1,24 @@
 #!/bin/bash
 
+build="release debug"
+
 if [ -z "$1" ]; then
-	echo "Usage: $0 version"
+	echo "Usage: $0 version [build]"
 	exit 1
 fi
 
 rel=$1
+[ -n "$2" ] && build="$2"
+
 set -x 
 
 branch=`git rev-parse --abbrev-ref HEAD`
 
 git checkout $rel
 
-for ver in release debug; do
+for ver in $build; do
 	dest=rel/$rel/$ver
-	[ -d $dest ] && continue
+	[ -d $dest -a -z "$FORCE" ] && continue
 	make mk_${ver}_defconfig
 	make -j 4
 	mkdir -p $dest
