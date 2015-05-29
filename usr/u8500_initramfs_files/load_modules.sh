76effb77e797301d17ada33939beb9fcdc94cf13
Marcin Kaluza <marcin.kaluza@trioptimum.com>
2015-05-30 00:15:13 +0200

    init: check for modules file in module_loader.sh
    does not show error in log if the file is not present

--- usr/u8500_initramfs_files/load_modules.sh
+++ usr/u8500_initramfs_files/load_modules.sh
@@ -1,4 +1,9 @@
 #! /system/bin/sh
+if [ ! -e /system/etc/modules ]; then
+	echo "Module loadedr: /system/etc/modules not found. Exiting"
+	exit 1
+fi
+
 modules=`cat /system/etc/modules | grep -v "#" | sed -e "s/[ \t]//g"`
 for m in $modules; do
 	mm=/system/lib/modules/${m}.ko
